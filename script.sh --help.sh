6a2e4fc93ac453a2c39a0d89d47ed54dfc2058eb
IharSt <dsw51769@student.dsw.edu.pl>
2024-04-15 10:26:03 +0200

    NewScrypt

---
+++ script.sh --help.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ echo "Usage: $0 [--logs <number_of_files>] [--help]"
+ echo "Options:"
+ echo "  --logs <number_of_files> : Create specified number of log files"
+ echo "  --help                   : Display this help message"
+
